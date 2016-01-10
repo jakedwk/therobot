64275cedff0349b05f2a1a1768099501ede5b534
jakedwk <dwk975@163.com>
2016-01-10 16:14:51 +0800

    20160110_1615 修改了main作为server发送图像及信息，接收端设计了qt程序

--- main/main.cpp
+++ main/main.cpp
@@ -2,34 +2,61 @@
 #include"../src/detecting.h"
 #include"../src/convey.h"
 
+
+void load_data(vector<int> &data,Detect* dt)
+{
+    data[0] = dt->dir;
+    data[1] = dt->ret.width;
+    data[2] = dt->ret.height;
+}
+
+
+
 int main( int argc, char** argv )
 {
-    //int fd;
-    //char a;
-    //char buf[300];
- 
-    //if((fd = uart_init((char*)"ttyACM0", 0)) <0)   //打开串口，波特率为115200；
-    //{
-        //printf("Open uart err \n");
-        //return -1;
-    //}
-    int sefd = 55,new_sockfd;
-    char ack;
+    bool serial_flag = false;
+    int serial_fd;
+    if((serial_fd = uart_init((char*)"ttyACM0", 0)) <0)   //打开串口，波特率为115200；
+    {
+        printf("Open uart err \n");
+        serial_flag = 1;
+    }
+    int sefd,new_sockfd;
+    char ack = 'o';
+    vector<int> data;
     Detect dt;
     Convey conv;
-    dt.detectinit(1);
+    dt.detectinit(-1);
     conv.socketinit(sefd);
     cout<<sefd<<endl;
-    new_sockfd = conv.accept_m(sefd);
-    cout<<new_sockfd<<endl;
+    data.resize(3);
     while(1)
     {
-        dt.detecting();
-        conv.sendimg(new_sockfd,dt.frame);
-        if(!conv.recvall(new_sockfd,&ack,1)) break;
+        cout<<"accepting!"<<endl;
+        new_sockfd = conv.accept_m(sefd);
+        cout<<new_sockfd<<endl;
+        while(1)
+        {
+            dt.detecting();
+            if(serial_flag) write(serial_fd,&dt.dir,4);
+            load_data(data,&dt);
+            conv.recvall(new_sockfd,&ack,1) ;
+            if(ack== 'q') break;
+            conv.sendimg(new_sockfd,dt.frame);
+            conv.senddata(new_sockfd,data,data.size());
+        }
+        close(new_sockfd);
+        if(ack == 's') break;
     }
-    close(new_sockfd);
     close(sefd);
+
+
+
+
+
+
+
+
     //sprintf(buf, "Hello world !\n");   //输出内容
   
     //while(1)
@@ -42,3 +69,5 @@ int main( int argc, char** argv )
     //}
     return 0;
 }
+
+
